49b7d258eb92bf2205cf8c6036ff1f1b9092f774
Steven Kay <stevendkay@gmail.com>
2018-11-27 20:46:35 +0000

    minor refactoring

--- src/views/instrument-families-info-view.js
+++ src/views/instrument-families-info-view.js
@@ -4,33 +4,35 @@ const InstrumentFamiliesInfoView = function (element) {
   this.element = element;
 };
 
+InstrumentFamiliesInfoView.prototype.showInfo = function (familyInfo) {
+  // extract fields
+
+  const name = familyInfo.name;
+  const description = familyInfo.description;
+
+  // get injection point
+  const infoDiv = document.querySelector("#instrument-family-info");
+
+  const nameDiv = document.createElement("div");
+  nameDiv.className = "name"
+  nameDiv.textContent = name
+
+  const descriptionDiv = document.createElement("div");
+  descriptionDiv.className = "description"
+  descriptionDiv.textContent = description;
+
+  // pop these into the injection point
+  infoDiv.appendChild(nameDiv);
+  infoDiv.appendChild(descriptionDiv);
+};
+
 InstrumentFamiliesInfoView.prototype.bindEvents = function () {
   // listen for selected family info being sent in
   PubSub.subscribe("InstrumentFamiliesModel:instrument-family-info", (event) => {
     PubSub.signForDelivery(this, event);
     event.preventDefault();
     const selectedFamilyInfo = event.detail;
-
-    // extract fields
-
-    const name = selectedFamilyInfo.name;
-    const description = selectedFamilyInfo.description;
-
-    // get injection point
-    const infoDiv = document.querySelector("#instrument-family-info");
-
-    const nameDiv = document.createElement("div");
-    nameDiv.className = "name"
-    nameDiv.textContent = name
-
-    const descriptionDiv = document.createElement("div");
-    descriptionDiv.className = "description"
-    descriptionDiv.textContent = description;
-
-    // pop these into the injection point
-    infoDiv.appendChild(nameDiv);
-    infoDiv.appendChild(descriptionDiv);
-
+    this.showInfo(selectedFamilyInfo);
   })
 
 
