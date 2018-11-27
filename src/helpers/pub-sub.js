2635999173f4c6a8bc177bd7a8d7c5e16228df17
Steven Kay <stevendkay@gmail.com>
2018-11-27 18:42:32 +0000

    add in my chatty version of pubsub to help with debugging and tracing progress

--- src/helpers/pub-sub.js
+++ src/helpers/pub-sub.js
@@ -1,14 +1,15 @@
+// these are like Ruby class (self.X) methods
+
 const PubSub = {
-  publish: function(channel, payload){
-    const event = new CustomEvent(channel, {
-      detail: payload
-    });
+  publish : function(channel, payload){
+    const event = new CustomEvent(channel, { detail: payload } );
+    console.log(`New event on ${channel} : ${ payload }`);
     document.dispatchEvent(event);
   },
-
   subscribe: function(channel, callback){
+    console.log(`New event on ${channel}`);
     document.addEventListener(channel, callback);
   }
-}
+};
 
 module.exports = PubSub;
